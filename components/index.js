f51e73aa41b9c69df7c22d915e300a16168d1d2d
Aaron Shafovaloff <aaronshaf@gmail.com>
2016-11-30 14:15:51 -0700

    WIP

--- components/index.js
+++ components/index.js
@@ -8,17 +8,18 @@ export default class Rate extends Component {
   constructor (props) {
     super(props)
     this.input = props.input
+    this.state.value = this.input.value
   }
 
   handleChange (value) {
-    this.input.value = value
+    this.setState({value})
   }
 
   render () {
     const starCount = Math.ceil(this.input.max || 5)
     const stars = []
     for (let x = 1; x <= starCount; x++) {
-      const fillPercentage = this.input.value < x ? 0 : 100
+      const fillPercentage = this.state.value < x ? 0 : 100
       stars.push(
         <Star
           key={x}
